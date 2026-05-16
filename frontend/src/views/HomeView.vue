9dfdc6312a74bc62cbfa6a86c393cc180d260789
Yesa Pritama <yesapritama565@gmail.com>
2026-05-16 14:32:21 +0700

    update frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <a href="/about">about</a>
+  </main>
+</template>
+
